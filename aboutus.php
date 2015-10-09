52a65da177b5751257dfec5fed91637508488d7b
Jeffrey Alvarado <jefalva10296@gmail.com>
2015-10-09 23:52:06 +0000

    Agregado registro de eventos

--- aboutus.php
+++ aboutus.php
@@ -12,22 +12,16 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 							Sobre MatchMe.com
 						</h1>
 						<p> 
-							<strong>Who We Are</strong> 
+							<strong>Motivacion</strong> 
 						</p>
 						<p> 
-							Launched on the Web in April of 1995, Match.com helped pioneer the online dating industry and now services 24 countries and territories and hosts Web sites in 15 different languages. We create romantic opportunities so singles are more likely to find someone special. Over the years, we've learned more and more about what people want—and the tools they need to help take the lottery out of love. With more ways than ever to bring singles together, we've come a long way since 1995. 
+							El proyecto tiene la proyeccion de junntar personas basadas en sus gustos e intereses, de manera indiscriminada y segura.
 						</p>
 						<p> 
-							<strong>What We Do</strong> 
-						</p>
-						<p> 
-							Our mission is simple: to help singles find the kind of relationship they're looking for. And we think we're pretty good at it. Every month, we hear from hundreds of success couples from all over the world—sharing love stories, sending invitations to weddings and announcing the births of new babies. It's no wonder everyone knows someone who's found love on Match.com! 
-						</p>
-						<p> 
-							<strong>How It Works</strong> 
+							<strong>Como funciona?</strong> 
 						</p>
 						<p>
-							 At Match.com, we give singles the opportunity to express themselves through various free writing sections. Profiles may include up to 26 photos, as well as selected preferences regarding the person they're searching for. With the click of a mouse, members can instantly see photos and read about potential matches in their area. Match.com also understands the importance of privacy and integrity. All communication between members on Match.com happens through an "anonymous" email network. The names and contact information of all our members are kept confidential until the member personally decides to share the information with a potential match. And to help ensure the integrity of our community, every profile and photo is screened by our Customer Care team for appropriateness before it's posted to the site. 
+							 Proveemos la posibilidad de, a travez del registro de datos comunes, hacer una pareja centrado en los intereses mas relevantes entre dos personas.
 						</p>
 						<div class="bnr-text"></div>
 					</div>		
